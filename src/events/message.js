5a6553d01d0699e4248a98cc0288a0db747d24d8
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-06 00:21:12 +0300

    rewrote algo to check for the command

--- src/events/message.js
+++ src/events/message.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const { MessageEmbed } = require('discord.js');
 let { bot } = require('../index');
 const config = require(ROOT_PATH + '/config.json');
 
@@ -6,45 +7,50 @@ bot.on('message', async (message) => {
     if (message.channel.type == 'dm' || message.author.bot == true || message.guild.id != config.guild || !message.content.startsWith(config.prefix)) return;
 
     let args = message.content.trim().slice(config.prefix.length).split(/ +/);
-    let command = args.shift().toLowerCase();
 
-    let cmd = bot.commands.find(x => x.name == command || x.alias.includes(command));
-    if (cmd != null) {
 
-        if (cmd.requiredPermission != null && !message.member.permissions.has(cmd.requiredPermission)) { // Permission check
-            message.channel.send(config.missing_permission.replace("{PERMISSION}", cmd.requiredPermission)); // Missing permission message
-            return;
-        }
+    let findCommand = (list, commandsList) => {
+        let cmd;
+        while (list.length > 0 && (cmd != null || args.length == list.length)) {
+            let command = list.shift().toLowerCase();
+            let temp;
 
-        if (cmd.subCommands != null) {
-            if (args[0] != null) {
-                let subCommand = args.shift().toLowerCase();
-                let subcmd = cmd.subCommands.find(x => x.name == subCommand || x.alias.includes(subCommand));
+            if (cmd != null && cmd.subCommands != null) temp = cmd.subCommands.find(x => x.name == command || x.aliases.includes(command));
+            else temp = commandsList.find(x => x.name == command || x.aliases.includes(command));
 
-                if (subcmd != null) {
-
-                    if (subcmd.requiredPermission != null && !message.member.permissions.has(subcmd.requiredPermission)) { // Permission check
-                        message.channel.send(config.missing_permission.replace("{PERMISSION}", subcmd.requiredPermission)); // Missing permission message
-                        return;
-                    }
-
-                    try {
-                        subcmd.run(bot, message, args);
-                    } catch (error) {
-                        console.log(chalk.bgRedBright("[ERROR]"), `An error occured while trying to execute the ${cmd.name}/${subcmd.name} command!`);
-                        console.log(error);
-                    }
-                } else {
-                    // If you want to return anything.
-                }
+            if (temp != null && temp.requiredPermission != null && !message.memberp.permissions.has(temp.requiredPermission)) { // Permission check
+                message.channel.send({ content: config.missing_permission.replace("{PERMISSION}", temp.requiredPermission) }); // Missing permission message
+                return;
             }
-        } else {
+
+            if (temp != null) cmd = temp;
+            if (temp == null || list.length == 0) return cmd;
+        }
+    }
+
+    let cmd = findCommand([...args], bot.commands);
+
+    if (cmd != null) {
+        let commandTree = args.slice(0, cmd.depth)
+        args = args.slice(cmd.depth);
+
+        if (cmd.subCommands == null) {
             try {
                 cmd.run(bot, message, args);
             } catch (error) {
                 console.log(chalk.bgRedBright("[ERROR]"), `An error occured while trying to execute the ${cmd.name} command!`);
                 console.log(error);
             }
+        } else {
+
+            message.channel.send({
+                embed: new MessageEmbed()
+                    .setColor("BLUE").setTitle(commandTree.join(" ") + "'s subcommands")
+                    .setDescription(cmd.subCommands.map(x => `**${x.name}** - \`${x.description}\`\n*usage:* \`${x.usage}\``).join('\n\n'))
+            })
+            // TODO Do subcommands list 
+
         }
+
     }
 });
