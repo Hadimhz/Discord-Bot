55ef3e34a714677c3fb5905b83ebf7ba8d7c6a11
Hadimhz <solo1010.mahfouz@gmail.com>
2021-07-07 01:19:48 +0300

    Moved find command to commandHandler

--- src/events/message.js
+++ src/events/message.js
@@ -2,33 +2,14 @@ const chalk = require('chalk');
 const { MessageEmbed } = require('discord.js');
 let { bot } = require('../index');
 const config = require(ROOT_PATH + '/config.json');
+const { findCommand } = require('../utils/commandHandler');
 
 bot.on('message', async (message) => {
     if (message.channel.type == 'dm' || message.author.bot == true || message.guild.id != config.guild || !message.content.startsWith(config.prefix)) return;
 
     let args = message.content.trim().slice(config.prefix.length).split(/ +/);
 
-
-    let findCommand = (list, commandsList) => {
-        let cmd;
-        while (list.length > 0 && (cmd != null || args.length == list.length)) {
-            let command = list.shift().toLowerCase();
-            let temp;
-
-            if (cmd != null && cmd.subCommands != null) temp = cmd.subCommands.find(x => x.name == command || x.aliases.includes(command));
-            else temp = commandsList.find(x => x.name == command || x.aliases.includes(command));
-
-            if (temp != null && temp.requiredPermission != null && !message.memberp.permissions.has(temp.requiredPermission)) { // Permission check
-                message.channel.send({ content: config.missing_permission.replace("{PERMISSION}", temp.requiredPermission) }); // Missing permission message
-                return;
-            }
-
-            if (temp != null) cmd = temp;
-            if (temp == null || list.length == 0) return cmd;
-        }
-    }
-
-    let cmd = findCommand([...args], bot.commands);
+    let cmd = findCommand([...args], bot.commands, message);
 
     if (cmd != null) {
         let commandTree = args.slice(0, cmd.depth)
@@ -45,10 +26,10 @@ bot.on('message', async (message) => {
 
             message.channel.send({
                 embed: new MessageEmbed()
-                    .setColor("BLUE").setTitle(commandTree.join(" ") + "'s subcommands")
+                    .setColor("PURPLE").setTitle(commandTree.join(" ") + "'s subcommands")
                     .setDescription(cmd.subCommands.map(x => `**${x.name}** - \`${x.description}\`\n*usage:* \`${x.usage}\``).join('\n\n'))
+                    .setFooter(`Executed by ${message.author.tag}`).setTimestamp()
             })
-            // TODO Do subcommands list 
 
         }
 
